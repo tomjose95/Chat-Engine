150375bcfde831f513f6456a4334f5c7ff3fa95e
Zypher <44130152+IceAge95@users.noreply.github.com>
2023-04-25 21:40:28 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const port = 8000;
 const path = require("path");
+const sassMiddleware = require("node-sass-middleware");
 const expressLayout = require("express-ejs-layouts");
 const app = express();
 app.use(expressLayout);
-
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
+app.use(express.static("./assets"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set("layouts extractStyles", true);
