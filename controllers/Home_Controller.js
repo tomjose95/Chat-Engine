fc6859660e415fe27d9d09d7c445e8b28620d381
Zypher <44130152+IceAge95@users.noreply.github.com>
2023-04-26 21:58:28 +0530

    frontend chat page

--- controllers/Home_Controller.js
+++ controllers/Home_Controller.js
@@ -1,3 +1,7 @@
 module.exports.home = (req, res) => {
   res.render("home", { title: "Home | Chat Engine" });
 };
+
+module.exports.chat = (req, res) => {
+  res.render("chat", { title: "Chat | Chat Engine" });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 console.log("Index router Loaded");
 const HomeController = require("./../controllers/Home_Controller");
 router.get("/", HomeController.home);
+router.get("/chat", HomeController.chat);
 module.exports = router;
